50102ae0955a725acdbdd4aeb84b721e35de075f
jorgegoco <jorgegoco70@gmail.com>
2023-03-01 18:25:20 +0100

    Add methods in Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
