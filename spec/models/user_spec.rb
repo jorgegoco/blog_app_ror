2e9e896c6b9701f191f0f3c66639ebe4d3e89eff
jorgegoco <jorgegoco70@gmail.com>
2023-03-02 12:46:35 +0100

    Add Rspec tests to user class

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'pepe', photo: 'pepe@hotmail.com', bio: 'Un malandro', posts_counter: 0) }
+  before { subject.save }
+  
+  it 'Subject should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be an integer' do
+    subject.posts_counter = '5.3'
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'three most recent posts from an user method ' do
+    Post.create(author: subject, title: 'post1', text: 'text1', comments_counter: 0, likes_counter: 0)
+    Post.create(author: subject, title: 'post2', text: 'text2', comments_counter: 0, likes_counter: 0)
+    Post.create(author: subject, title: 'post3', text: 'text3', comments_counter: 0, likes_counter: 0)
+    Post.create(author: subject, title: 'post4', text: 'text4', comments_counter: 0, likes_counter: 0)
+    expect(subject.three_recent_posts).to eq(subject.posts.order(updated_at: :desc).limit(3))
+    expect(subject.three_recent_posts.length).to eq 3
+  end
+end
