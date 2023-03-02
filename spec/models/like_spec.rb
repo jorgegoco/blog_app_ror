c67436053282999c9ade8902452d973baa629a81
jorgegoco <jorgegoco70@gmail.com>
2023-03-02 18:21:32 +0100

    Add Rspec tests to Like class

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  subject do
+    user = User.create(name: 'pepe', photo: 'pepe@hotmail.com', bio: 'Un malandro', posts_counter: 0)
+    user1 = User.create(name: 'potamo', photo: 'potamo@cool.com', bio: 'Otro malandro', posts_counter: 0)                  
+    post = Post.create(title: 'post1', text: 'text1', comments_counter: 0, likes_counter: 0, author: user)
+    Like.create(post: post, author: user1)
+    Like.create(post: post, author: user1)
+  end
+
+  it 'likes should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'should update likes_counter' do
+    expect(subject.post.likes_counter).to eq 2
+  end
+end
