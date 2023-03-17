ccb0dda9db49c5b2a96de3e795742f0fbe22c8dc
jorgegoco <jorgegoco70@gmail.com>
2023-03-17 10:46:04 +0100

    Add suggested changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,17 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    # can :read, :all
-
-    # return unless user.present?
-
-    # can :manage, Post, author_id: user.id
-    # can :manage, Comment, author_id: user.id
-    # can :manage, Like, author_id: user.id
-    # return unless user.admin?
-
-    # can :manage, :all
-
     can :read, :all
     return unless user.present?
 
