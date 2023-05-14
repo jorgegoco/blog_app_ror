295475ece19c8d165f35df86c67acd0075b39f22
jorgegoco <jorgegoco70@gmail.com>
2023-05-14 11:58:38 +0200

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
+  namespace :api do
+    resources :users, only: [] do
+      resources :posts, only: [:index, :show]
+    end
+  end
 end
